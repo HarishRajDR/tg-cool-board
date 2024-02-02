aa5153adc8f6ffb23565c3f77fc1af57c69ae16b
Harish Raj D R <drharishrajdr@gmail.com>
2024-02-02 19:28:49 +0530

    Added index page with details

--- routes/index.ts
+++ routes/index.ts
@@ -1,3 +1,11 @@
 export default eventHandler(() => {
-  return { nitro: 'Is Awesome!' }
-})
+  return {
+    welcome: "Welcome to the Top gear Cool Wall API",
+    description:
+      "The Cool Wall, introduced in the sixth episode of Series 1, was a wall on which Top Gear presenters Jeremy Clarkson and Richard Hammond decided which cars were cool and which were not by placing photographs of them on to various sections of the wall. The categories were, from left to right; 'Seriously Uncool', 'Uncool', 'Cool', and 'Sub Zero'.",
+    dataTakenFrom: "https://topgear.fandom.com/wiki/The_Cool_Wall",
+    status: "Operational",
+    projectLink: "https://github.com/HarishRajDR/tg-cool-board",
+    author: "Harish Raj D R ⚡",
+  };
+});
